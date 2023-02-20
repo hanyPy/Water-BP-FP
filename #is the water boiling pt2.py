ec671f34fdf9d9048f534d9c2b6e74d4cb58373e
hanyPy <125481007+hanyPy@users.noreply.github.com>
2023-02-19 23:18:06 -0600

    Add files via upload

---
+++ #is the water boiling pt2.py
@@ -0,0 +1,18 @@
+                          #is the water boiling ? Let's find out
+###ONGOING PROJECT                     
+##FAHRENHEIT TBD
+temprature = int(input("Write the temprature of the water: "))
+cof = str(input("Write either c or f: "))
+if cof == str("c" or "C" or "f" "F"):
+    print("The temprature at " + str(temprature)+str(cof)+" is: ")
+else:
+   print("Invalid sign.\n " + str(input("Write EITHER C or F : ")))
+
+
+
+
+                                 #celsuis
+if int(temprature) >= 100:
+    print("The water at " + str(temprature)+ "C" " is at boiling point.")
+else:
+    print("The water at " + str(temprature)+ "C" " is not at boiling point.")
---
+++ Is the water boiling.py
@@ -0,0 +1,42 @@
+#Is the water boiling?   Let us find out
+                                    #ONGOING PROJECT
+
+list= str(["a","b"])
+print("Write the temprature of the water down below.")
+
+#Temprature and sign
+temprature = input()
+sign = str(input("C or F: "))
+
+if sign ==list:
+  print("Invalid sign.")
+if temprature == str:
+  print("Invalid number")
+
+signc = ("C")
+signf = ("F")
+
+
+
+                                                         #FAHRENHEIT
+
+if temprature == int(temprature)>= int(212):
+   print("The water at " + str(temprature) +signf + " is boiling.")
+else:
+   print("The water at " + str(temprature) + signf + " is not boiling.")
+
+
+    
+                                                              #CELSIUS
+   
+
+if temprature== int(temprature)>= 100 and temprature== str:
+    print("The water at " + str(temprature) + signc + " is boiling.")
+
+if signc== int(temprature)<= 212 and temprature== str:
+    print("The water at " + str(temprature) + signc + " is boiling.")
+
+
+    #END OF CODE
+        #  #ENJOY
+#fix errors tomorrow.
